1c56c028e77538605341ccc96b02a7fd0dbbfea8
Matthew Abel <74427201+MatthewCAbel@users.noreply.github.com>
2024-12-03 20:59:39 +0000

    Update WeaponBuffs.lua

--- mods/Buff Backports/lua/WeaponBuffs.lua
+++ mods/Buff Backports/lua/WeaponBuffs.lua
@@ -182,7 +182,13 @@ self.deagle.fire_mode_data.fire_rate = 60 / 200
 self.deagle.stats.damage = 123 
 self.deagle.AMMO_PICKUP = {1, 2} 
 
-self.deagle.stats_modifiers = {
+self.x_deagle.CLIP_AMMO_MAX = 14 
+self.x_deagle.AMMO_MAX = 56
+self.deagle.fire_mode_data.fire_rate = 60 / 200
+self.deagle.stats.damage = 123 
+self.deagle.AMMO_PICKUP = {2, 4} 
+
+self.x_deagle.stats_modifiers = {
 		damage = 2
 }	
 
@@ -193,6 +199,16 @@ self.mateba.AMMO_PICKUP = {1, 2}
 self.mateba.stats.reload = 14
 
 self.mateba.stats_modifiers = {
+		damage = 2
+}
+
+self.x_mateba.CLIP_AMMO_MAX = 12
+self.x_mateba.AMMO_MAX = 60
+self.x_mateba.stats.damage = 123 
+self.x_mateba.AMMO_PICKUP = {2, 4} 
+self.x_mateba.stats.reload = 14
+
+self.x_mateba.stats_modifiers = {
 		damage = 2
 }
 end )
